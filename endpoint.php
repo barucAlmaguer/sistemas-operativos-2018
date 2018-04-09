ca0900a57559103b5c7b7781849efd55d7b6659c
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-04-08 19:49:27 -0500

    changed db connection string

--- endpoint.php
+++ endpoint.php
@@ -1,7 +1,17 @@
 <?php
 $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str);
-$conn = new mysqli('localhost', 'root', '1234', 'testApp');
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
 $sql = "SELECT * FROM USUARIOS WHERE username = '".$json_obj->{'username'}."'";
 $result = $conn->query($sql);
 $outp = $result->fetch_all(MYSQLI_ASSOC);
