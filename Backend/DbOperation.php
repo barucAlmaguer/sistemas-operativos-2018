504ee8b443c6878fed8e509c8ebf0fb393817071
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-04-19 18:05:29 -0500

    Backend Folder

---
+++ Backend/DbOperation.php
@@ -0,0 +1,21 @@
+<?php
+
+class DbOperation{
+    private $conn;
+    function __construct(){
+        require_once "DbConnect.php";
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+    function getUsers($username, $password){
+      $sql = "SELECT * FROM USUARIOS WHERE username = ? AND password = ?";
+      $stmt = $this->conn->prepare($sql);
+      $stmt->bind_param("ss", $username, $password);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      return  $result;
+    }
+}
+
+?>
