b09be3ef6e26905be2c45116d3791b8a36d6f490
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-04-19 18:38:35 -0500

    "/Backend/getUsers.php: modificaciones"

--- Backend/getMaterias.php
+++ Backend/getMaterias.php
@@ -2,15 +2,12 @@
 $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str);
 
-$username = $json_obj->{'username'};
-$password = $json_obj->{'password'};
-
 require_once "DbOperation.php";
 $dbo = new DbOperation();
 
-$result = $dbo->getUsers($username, $password);
+$result = $dbo->getMaterias();
 $outp = array();
-$outp['usuarios'] = $result->fetch_all(MYSQLI_ASSOC);
+$outp['materias'] = $result->fetch_all(MYSQLI_ASSOC);
 
 echo json_encode($outp);
 $result->free();
