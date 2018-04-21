37df800cae6ad317cb7b3884b8fba11bb0012afb
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-04-21 13:41:02 -0500

    test getCursos(101);

--- Backend/getCursos.php
+++ Backend/getCursos.php
@@ -7,7 +7,7 @@ $materia = $json_obj->{'materia'};
 require_once "DbOperation.php";
 $dbo = new DbOperation();
 
-$result = $dbo->getUsers($materia);
+$result = $dbo->getUsers(101);
 $outp = $result->fetch_all(MYSQLI_ASSOC);
 
 echo json_encode($outp);
