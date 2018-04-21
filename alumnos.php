2bf46cf07d7c97851bd86b95c78247602b441a87
MichelBecerra <michelandreabc@gmail.com>
2018-04-21 18:48:43 -0500

    adding alumnos php

---
+++ alumnos.php
@@ -0,0 +1,24 @@
+<?php
+$json_str = file_get_contents('php://input');
+$json_obj = json_decode($json_str);
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+$sql = "SELECT estudiantes.matricula, estudiantes.nombre
+        from materiaestudiantes
+        inner join estudiantes on materiaestudiantes.estudiantes_id = estudiantes.id_estudiante
+        where materiaestudiantes.materia_id = $materia";
+
+$result = $conn->query($sql);
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+echo json_encode($outp) ;
+$conn->close();
+?>
